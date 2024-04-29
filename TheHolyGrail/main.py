e097379993d389041d9de66971b78c6d7afced50
Amaan <amaan.brs@gmail.com>
2024-04-29 11:56:04 +0300

    Heart rate sensor not detecting heart rate

--- TheHolyGrail/main.py
+++ TheHolyGrail/main.py
@@ -5,6 +5,7 @@ from filefifo import Filefifo
 from fifo import Fifo
 from led import Led
 from piotimer import Piotimer
+import micropython
 #import extraction
 
 
@@ -22,7 +23,8 @@ class Encoder:
         else:
             self.fifo.put(+1)
             
- 
+
+micropython.alloc_emergency_exception_buf(200)
 samples = Fifo(2000)
 press = Fifo(20)
 sample_list = []
@@ -74,7 +76,7 @@ def start_menu():
         oled.show()
         if press.has_data():
             value = press.get()
-            if ts - pts < 200:
+            if ts - pts < 250:
                 print(ts - pts)
                 pts = ts
                 continue
@@ -103,11 +105,10 @@ def start_menu():
             
 
 
-def get_signal():
-    data = sensor.read_u16()
-    samples.put(data)
+def get_signal(tid):
+    samples.put(sensor.read_u16())
 
-timer = Piotimer(period = gap_ms, mode = Piotimer.PERIODIC, callback = get_signal)
+timer = Piotimer(period = 4, mode = Piotimer.PERIODIC, callback = get_signal)
 
             
             
@@ -136,40 +137,40 @@ pts = ts
 start_menu()
 
 
-
 if highlighted_text == 1:
     while True:
         if samples.has_data():
-            print("But this is where the problem arises")
             sample = samples.get()
             sample_list.append(sample)
             count += 1
             if sample < 0:
                 break
             if count >= 750:
-                max = max(sample_list)
-                min = min(sample_list)
-                threshhold = (4*max + min)/5
-                prev = samples[0]
+                max_value = max(sample_list)
+                min_value = min(sample_list)
+                threshhold = (4*max_value + min_value)/5
+                print(max_value)
+                #prev = sample_list[0]
+                print(threshhold)
                 counter = 0
                 
                 #gathering peak counts
                 for i in sample_list:
                     if i >= threshhold and i > max_sample:
                         max_sample = i
-                    if i < threshold and max_sample != 0:
+                    if i < threshhold and max_sample != 0:
                         peakcounts.append(sample_list.index(max_sample))
                         max_sample = 0
                         
                         
-                for i in range(len(peak)):
+                for i in range(len(peakcounts)):
                     delta = peakcounts[i] - peakcounts [i - 1]
                     ppi = delta * gap_ms
                     
                     if ppi > 0:
                         heartrate = 60000/ppi
                         heartrate = round(heartrate)
-                        if hearate > minhr and heartrate < maxhr:
+                        if heartrate > minhr and heartrate < maxhr:
                             oled.fill(0)
                             oled.text(f"HR : {heartrate} BPM", 52, 30, 1)
                             oled.show()

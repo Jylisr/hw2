dc755792ef85f28567898fab4d0a7f39b346e310
Amaan <amaan.brs@gmail.com>
2024-05-05 22:12:03 +0300

    Changes in mqtt and kubios

--- TheHolyGrail/kubios.py
+++ TheHolyGrail/kubios.py
@@ -4,42 +4,46 @@ import network
 from time import sleep
 from main import ppis
 
-APIKEY = "pbZRUi49X48I56oL1Lq8y8NDjq6rPfzX3AQeNo3a"
-CLIENT_ID = "3pjgjdmamlj759te85icf0lucv"
-CLIENT_SECRET = "111fqsli1eo7mejcrlffbklvftcnfl4keoadrdv1o45vt9pndlef"
-LOGIN_URL = "https://kubioscloud.auth.eu-west-1.amazoncognito.com/login"
-TOKEN_URL = "https://kubioscloud.auth.eu-west-1.amazoncognito.com/oauth2/token"
-REDIRECT_URI = "https://analysis.kubioscloud.com/v1/portal/login"
+def Kubios(ppis):
+    APIKEY = "pbZRUi49X48I56oL1Lq8y8NDjq6rPfzX3AQeNo3a"
+    CLIENT_ID = "3pjgjdmamlj759te85icf0lucv"
+    CLIENT_SECRET = "111fqsli1eo7mejcrlffbklvftcnfl4keoadrdv1o45vt9pndlef"
+    LOGIN_URL = "https://kubioscloud.auth.eu-west-1.amazoncognito.com/login"
+    TOKEN_URL = "https://kubioscloud.auth.eu-west-1.amazoncognito.com/oauth2/token"
+    REDIRECT_URI = "https://analysis.kubioscloud.com/v1/portal/login"
 
-response = requests.post(
- url = TOKEN_URL,
- data = 'grant_type=client_credentials&client_id={}'.format(CLIENT_ID),
- headers = {'Content-Type':'application/x-www-form-urlencoded'},
- auth = (CLIENT_ID, CLIENT_SECRET))
-response = response.json() #Parse JSON response into a python dictionary
+    response = requests.post(
+     url = TOKEN_URL,
+     data = 'grant_type=client_credentials&client_id={}'.format(CLIENT_ID),
+     headers = {'Content-Type':'application/x-www-form-urlencoded'},
+     auth = (CLIENT_ID, CLIENT_SECRET))
+    response = response.json() #Parse JSON response into a python dictionary
 
-access_token = response["access_token"] #Parse access token
+    access_token = response["access_token"] #Parse access token
 
-#Interval data to be sent to Kubios Cloud. Replace with your own data:
+    #Interval data to be sent to Kubios Cloud. Replace with your own data:
 
-intervals = ppis
+    intervals = ppis
 
-#Create the dataset dictionary HERE
+    #Create the dataset dictionary HERE
+    dataset = {
+        "type": "RRI",
+        "data": intervals,
+        "analysis": {
+            "type": "readiness"
+            }
+        } 
+    
+    #Make the readiness analysis with the given data
 
-#Make the readiness analysis with the given data
+    response = requests.post(
+     url = "https://analysis.kubioscloud.com/v2/analytics/analyze",
+     headers = { "Authorization": "Bearer {}".format(access_token), "X-Api-Key": APIKEY}, json = dataset) 
+    response = response.json()
+    #Print out the SNS and PNS values on the OLED screen here
 
-response = requests.post(
- url = "https://analysis.kubioscloud.com/v2/analytics/analyze",s
- headers = { "Authorization": "Bearer {}".format(access_token),
-#use access token to access your Kubios Cloud analysis session
-
- "X-Api-Key": APIKEY},
- json = dataset) #dataset will be automatically converted to JSON by the urequests
-library
-response = response.json()
-#Print out the SNS and PNS values on the OLED screen here
-
-
+    pns = response['analysis']['pns_index']
+    sns = response['analysis']['sns_index']
 
 
 #The data sent to Kubios is the peak-to-peak intervals in milliseconds
--- TheHolyGrail/main.py
+++ TheHolyGrail/main.py
@@ -6,6 +6,7 @@ from fifo import Fifo
 from led import Led
 from piotimer import Piotimer
 import micropython
+from kubios import Kubios
 import ujson
 import os
 #import extraction
@@ -47,7 +48,7 @@ oled = SSD1306_I2C(oled_width, oled_height, i2c)
 sensor = ADC(Pin(26)) #ADC_0
 rot = Encoder(10,11)
 rot_butt = Pin(12, Pin.IN, pull = Pin.PULL_UP)
-info = 0
+
 def button_handler(pin):
     global ts
     ts = time.ticks_ms()
@@ -58,9 +59,7 @@ rot_butt.irq(handler = button_handler, trigger = Pin.IRQ_FALLING, hard = True)
 
 
 def start_menu():
-    global pts
-    global highlighted_text
-    global max_sample
+    global pts, highlighted_text, max_sample
     text_pos_magn = [0, 2, 4, 6]
     highlighted_text = 0
     print("Into start_menu")
@@ -261,45 +260,6 @@ def history():
                         oled.text(data[i], 0, (text_height * text_pos_magn[i]) + 1, 1)
                     oled.show()
 
-def kubios():
-    APIKEY = "pbZRUi49X48I56oL1Lq8y8NDjq6rPfzX3AQeNo3a"
-    CLIENT_ID = "3pjgjdmamlj759te85icf0lucv"
-    CLIENT_SECRET = "111fqsli1eo7mejcrlffbklvftcnfl4keoadrdv1o45vt9pndlef"
-    LOGIN_URL = "https://kubioscloud.auth.eu-west-1.amazoncognito.com/login"
-    TOKEN_URL = "https://kubioscloud.auth.eu-west-1.amazoncognito.com/oauth2/token"
-    REDIRECT_URI = "https://analysis.kubioscloud.com/v1/portal/login"
-
-    response = requests.post(
-     url = TOKEN_URL,
-     data = 'grant_type=client_credentials&client_id={}'.format(CLIENT_ID),
-     headers = {'Content-Type':'application/x-www-form-urlencoded'},
-     auth = (CLIENT_ID, CLIENT_SECRET))
-    response = response.json() #Parse JSON response into a python dictionary
-
-    access_token = response["access_token"] #Parse access token
-
-    #Interval data to be sent to Kubios Cloud. Replace with your own data:
-
-    intervals = ppis
-
-    #Create the dataset dictionary HERE
-    #Make the readiness analysis with the given data
-
-    response = requests.post(
-     url = "https://analysis.kubioscloud.com/v2/analytics/analyze",s
-     headers = { "Authorization": "Bearer {}".format(access_token),
-    #use access token to access your Kubios Cloud analysis session
-
-     "X-Api-Key": APIKEY},
-     json = dataset) #dataset will be automatically converted to JSON by the urequests
-    library
-    response = response.json()
-    #Print out the SNS and PNS values on the OLED screen here
-
-
-
-
-#The data sent to Kubios is the peak-to-peak intervals in milliseconds
 
 
             
@@ -366,6 +326,6 @@ while True:
     if highlighted_text == 3:
         history()
     if highlighted_text == 4:
-        data_works()
+        Kubios(ppis)
 
 
